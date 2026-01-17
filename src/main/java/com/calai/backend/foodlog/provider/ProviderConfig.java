bb267f8db5f03b956e52d9d1dcd48162e10d3a40
kurt <st9360606@gmail.com>
2026-01-18 01:15:56 +0800

    Gemini Step 5-06

--- src/main/java/com/calai/backend/foodlog/provider/ProviderConfig.java
+++ src/main/java/com/calai/backend/foodlog/provider/ProviderConfig.java
@@ -1,6 +1,5 @@
 package com.calai.backend.foodlog.provider;
 
-import com.calai.backend.foodlog.service.LogMealTokenService;
 import com.calai.backend.foodlog.task.ProviderClient;
 import com.calai.backend.foodlog.task.StubProviderClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,7 +11,7 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@EnableConfigurationProperties({LogMealProperties.class, GeminiProperties.class})
+@EnableConfigurationProperties({GeminiProperties.class})
 public class ProviderConfig {
 
     @Bean
@@ -43,27 +42,4 @@ public class ProviderConfig {
         return new GeminiProviderClient(geminiRestClient, props, om);
     }
 
-    // ---- LOGMEAL (先留到 Step 5 再移除) ----
-    @Bean
-    @ConditionalOnProperty(name = "app.foodlog.provider", havingValue = "LOGMEAL")
-    public RestClient logMealRestClient(LogMealProperties props) {
-        SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
-        f.setConnectTimeout((int) props.connectTimeoutOrDefault().toMillis());
-        f.setReadTimeout((int) props.readTimeoutOrDefault().toMillis());
-
-        return RestClient.builder()
-                .baseUrl(props.baseUrl())
-                .requestFactory(f)
-                .build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "app.foodlog.provider", havingValue = "LOGMEAL")
-    public ProviderClient logMealProviderClient(
-            RestClient logMealRestClient,
-            LogMealProperties props,
-            LogMealTokenService tokenService
-    ) {
-        return new LogMealProviderClient(logMealRestClient, props, tokenService);
-    }
 }
